edd61c3bd4a2f1aa70ac3de80c807af3119fae3f
meyacine <meyacine@meyacine-PC.fr01.awl.atosorigin.net>
2014-08-01 14:12:55 +0200

    [MYM] test initialisation

---
+++ src/test/java/com/technoSofTeam/best/batch/BatchLauncherTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.technoSofTeam.best.batch;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ * Test Class for the csv to mysql batch
+ * @author meyacine
+ */
+public class BatchLauncherTest extends AbstractTestNGSpringContextTests{
+    
+}
